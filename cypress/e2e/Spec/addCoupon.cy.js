ec22c8e7c6a53817584fae9d6744cf43e31eb1f8
Sohail Khan <sohailkhan@Sohails-MacBook-Pro-2.local>
2024-05-30 13:42:29 +0530

    add coupon

--- cypress/e2e/Spec/addCoupon.cy.js
+++ cypress/e2e/Spec/addCoupon.cy.js
@@ -1,18 +1,9 @@
-import { LoginPage } from "../pages/loginAdminPage.js";
+import { LoginPageFeatures } from "../../features/Auth/login";
+import { couponFeatures } from "../../features/Coupons/coupons_features";
+describe('Create coupon', () => {
+    it ('add coupon', () =>{
+        LoginPageFeatures.login()
+        couponFeatures.coupon()
+})
 
-describe('Login Tests', () => {
-    it('login', () => {
-        const loginPage = new LoginPage();
-
-        // Access environment variables directly
-        const BASE_URL = Cypress.env('BASE_URL');
-        const Email = Cypress.env('Email');
-        const Password = Cypress.env('Password');
-
-        cy.visit(BASE_URL);
-        loginPage.enterUsername(Email);
-        loginPage.enterPassword(Password);
-        loginPage.clickContinue();
-        cy.url().should('eq', 'https://spoon-admin.x-studio.io/dashboard');
-    });
-});
+})
---
+++ cypress/features/Coupons/coupons_features.js
@@ -0,0 +1,20 @@
+import { couponPage } from "../../e2e/pages/couponPage";
+import { faker } from "@faker-js/faker";
+const name =faker.name.findName()
+export const couponFeatures= {
+    coupon: () => {
+        cy.get(couponPage.coupons).click(); 
+        cy.get(couponPage.addCoupons).click();
+        cy.get(couponPage.couponCode).type(faker.random.alphaNumeric('10')); // Note the correction here
+        cy.get(couponPage.discountPercentage).type(Math.floor(Math.random() * 101)); // Note the correction here
+        cy.get(couponPage.discountLimit).type(faker.lorem.words(3));
+        cy.get(couponPage.minimumCartValue).type(faker.datatype.number({ min: 10, max: 100 }));
+        cy.get(couponPage.startDate).type(format(faker.date.birthdate(), 'yyyy-MM-dd')); // Note the correction here
+        cy.get(couponPage.endDate).type(format(faker.date.future(), 'yyyy-MM-dd')); // Note the correction here
+        cy.get(couponPage.couponSubmitButton).click();
+    }
+
+    
+
+
+}
